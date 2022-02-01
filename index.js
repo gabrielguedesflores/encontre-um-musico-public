94d45d3830e9d8dd1cc48618965ec4efecbbb188
mateusVedoy <mateusvedoy@outlook.com>
2022-02-01 00:10:40 -0300

    Construção do leiatute do login.html

--- index.js
+++ index.js
@@ -26,4 +26,15 @@ app.get("/sair", function(req, res){
   res.sendFile(__dirname + '/views/generic.html');
 });
 
-app.listen(process.env.PORT || 3000);
+app.get("/login", function(_, res){
+  res.sendFile(__dirname + '/views/login-register/login.html');
+});
+
+app.get("/cadastro", function(_, res){
+  res.sendFile(__dirname + '/views/login-register/cadastro.html');
+});
+
+// app.listen(process.env.PORT || 3000);
+app.listen(8081, () => {
+ console.log("RUNNING AT 8081")
+});
