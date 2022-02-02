a02805510ec42972ee1d637a0ec760d550894012
Gabriel Guedes Flores <gabriel.flores@e-unicred.com.br>
2022-02-02 20:18:12 -0300

    deploy do arquivo  controller.js

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -1,3 +1,37 @@
 $(document).ready(function(){
-  console.log('chegou')
+  console.log('chegou');
+  validaLogin()
 });
+
+const validaLogin = async () => { 
+  const user_id = 8; //aqui vai vir dinâmico depois
+  if(await getUser(user_id)){
+    showAllPage();
+  }else{
+    hideAllPage();
+  }
+}
+
+const getUserData = async (user_id) => {
+  const urlGetUser = `https://encontre-um-musico-api.herokuapp.com/api/users/${user_id}`;
+
+  try {
+    let { data } = await axios.get(urlGetUser, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      }
+    });
+    return data
+  } catch (error) {
+    console.log('Você não está logado!')
+    return false
+  } 
+}
+
+const showAllPage = () => {
+  $('#wrapper').show()
+}
+
+const hideAllPage = () => {
+  $('#wrapper').hide()
+}
