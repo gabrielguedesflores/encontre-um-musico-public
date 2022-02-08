d06688328d9dd8ee5f291f999528db81e511c474
Gabriel Guedes Flores <gabriel.flores@e-unicred.com.br>
2022-02-07 22:08:51 -0300

    inserindo dados dinamicos no frontend

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -1,34 +1,45 @@
 $(document).ready(function(){
-  console.log('chegou');
-  validaLogin()
+  validateLogin()
 });
 
-const validaLogin = async () => { 
-  const user_id = 8; //aqui vai vir dinâmico depois
-  if(await getUser(user_id)){
-    showAllPage();
+const validateLogin = async () => { 
+  const user_id = 16; //aqui vai vir dinâmico depois
+  const userData = await getUserFilterId(user_id)
+  if(userData != false){
+    showAllPage(userData);
   }else{
     hideAllPage();
   }
 }
 
-const getUserData = async (user_id) => {
-  const urlGetUser = `https://encontre-um-musico-api.herokuapp.com/api/users/${user_id}`;
-
+const getUserFilterId = async (user_id) => {
+  let urlUser = `https://encontre-um-musico-api.herokuapp.com/api/users/${user_id}`;
   try {
-    let { data } = await axios.get(urlGetUser, {
+    const { data } = await axios.get(urlUser, {
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
       }
     });
-    return data
+    return data;
   } catch (error) {
-    console.log('Você não está logado!')
+    toastr.error("Falha ao buscar os dados do usuário.", "Entre em contato com os Administradores!");
     return false
-  } 
+  }
 }
 
-const showAllPage = () => {
+const showAllPage = (userData) => {
+  const user_title = `<h1>${userData[0].user_title}</h1>`;
+  const user_full_name = `<strong>${userData[0].user_full_name}</strong>`
+  const user_adress = `<p>${userData[0].user_city} - ${userData[0].user_state}</p>`;
+  const user_bio = `<p>${userData[0].user_bio}</p>`;
+  const user_image = `<img src="${userData[0].user_image}" alt="" />`;
+
+  $('#user_title').append(user_title);
+  $('#user_full_name').append(user_full_name);
+  $('#user_adress').append(user_adress);
+  $('#user_bio').append(user_bio);
+  $('#user_image').append(user_image);
+
   $('#wrapper').show()
 }
 
