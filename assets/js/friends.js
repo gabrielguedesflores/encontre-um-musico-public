b8a39dad08926635cb01ad984e38c25db1bd5fae
Gabriel Guedes Flores <gabriel.flores@e-unicred.com.br>
2022-02-19 17:08:55 -0300

    add amigos dinamicos

--- assets/js/friends.js
+++ assets/js/friends.js
@@ -31,18 +31,16 @@ function getCookie(cname) {
 
 const validateLogin = async () => { 
   const user_id = user(); 
-  console.log(user_id);
-  const userData = await getUserFilterId(user_id)
-  const userFriends
-  if(userData != false){
-    showAllPage(userData);
+  const userFriends = await getFriendsOfUser(user_id)
+  if(userFriends){
+    showAllPage(userFriends);
   }else{
     toastr.error("Não possível encontrar seus amigos.")
   }
 }
 
 const getFriendsOfUser = async (user_id) => {
-  let urlUser = `https://encontre-um-musico-api.herokuapp.com/api/user/friends`;
+  let urlUser = 'https://encontre-um-musico-api.herokuapp.com/api/user/friends';
   try {
     const { data } = await axios.post(urlUser, {
       user_id: user_id
@@ -56,17 +54,24 @@ const getFriendsOfUser = async (user_id) => {
 
 const showAllPage = async (userData) => {
 
-  const user_title = `<h1>${userData[0].user_title}</h1>`;
-  const user_full_name = `<strong>${userData[0].user_full_name}</strong>`
-  const user_adress = `<p>${userData[0].user_city} - ${userData[0].user_state}</p>`;
-  const user_bio = `<p>${userData[0].user_bio}</p>`;
-  const user_image = `<img src="${userData[0].user_image}" alt="" />`;
+  const linha = `<hr class="major" />`;
+  for (let i = 0; i < userData.user_friends[0].user_friends.length; i++) {
+    const user_friend = `
+      <article>
+        <a href="#" class="image"><img src="${userData.user_friends[0].user_friends[i].user_image}" alt="" /></a>
+        <h3 style="color: #141115;">
+          ${userData.user_friends[0].user_friends[i].user_full_name}
+        </h3>
+        <p>${userData.user_friends[0].user_friends[i].user_bio}</p>
+        <ul class="actions">
+          <li><a href="#" class="button">Ver perfil</a></li>
+        </ul>
+      </article>
+  `;
+  $('.posts').append(user_friend);
+  $('.posts').append(linha);
 
-  $('#user_title').append(user_title);
-  $('#user_full_name').append(user_full_name);
-  $('#user_adress').append(user_adress);
-  $('#user_bio').append(user_bio);
-  $('#user_image').append(user_image);
+  }
 
   $('#wrapper').show()
 }
